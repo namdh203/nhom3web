bd3529af8e4f708c12dbed2cdd0fa27e330ebbc1
ZeroGxMax <21020554@vnu.edu.vn>
2023-12-10 18:25:10 +0700

    Info_summary updated

--- client/src/components/payment/info_summary/info_summary.jsx
+++ client/src/components/payment/info_summary/info_summary.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./info_summary.css";
+import Layout from "antd/es/layout/layout";
+import { Typography } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Paragraph, Title } = Typography;
 
 export default class TourSummary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tourName: "Family Beach Retreat in Gold Coast",
-      description: "family trip",
+      description: "Family trip",
       startDate: "20/10/2023",
       totalCost: 328,
       currency: "$",
@@ -17,41 +22,44 @@ export default class TourSummary extends React.Component {
     const { tourName, description, startDate, totalCost, currency } =
       this.state;
     return (
-      <div className="">
-        <div className="row justify-content-center">
-        <div className="block" style={{ height: '200px' }}>
+      <Layout>
+        <Header></Header>
+        <Content>
+          <div className="row justify-content-center">
+            <div className="bg-summary col-6 border border-secondary rounded my-5">
+              <Title level={5} className="text-center m-1" strong="true">
+                {tourName}
+              </Title>
+              <Paragraph className="text-center" type="secondary">
+                {description} on {startDate}
+              </Paragraph>
+              <div className="">
+                <div className="m-0 d-flex flex-row justify-content-between">
+                  <div>
+                  <Title className="m-0" type="secondary" level={4}>
+                    Total cost
+                    
+                  </Title>
+                  <Paragraph className="m-0" type="secondary">
+                    inclusive of all taxes and fees.
+                  </Paragraph>
+                  </div>
+                  
 
-        </div>
+                  <Title className="align-self-end" level={3} style={
+                    {
 
-          <div className="bg-summary col-6 border border-secondary rounded">
-            <div className="text-center">{tourName}</div>
-            <div className="text-center">{description} on {startDate}</div>
-            <div>Total cost: {currency} {totalCost}</div>
+                    }
+                  }>
+                    {totalCost} {currency}
+                  </Title>
+                  
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        {/* <p>
-                    {tourName}
-                </p>
-                <Paragraph>
-                    {tourName}
-                </Paragraph>
-                <Paragraph>
-                    {tourName}
-                </Paragraph>
-                <Paragraph>
-                    {tourName}
-                </Paragraph>
-                <Paragraph>
-                    {description} on {startDate}
-                </Paragraph>
-                <Paragraph>
-                Total cost: {currency}{totalCost}
-                </Paragraph>
-                <Paragraph>
-                    I have read and understood the Terms & Conditions
-                </Paragraph> */}
-      </div>
+        </Content>
+      </Layout>
     );
   }
 }
--- client/src/components/site_info/accommodation/accom.js
+++ client/src/components/site_info/accommodation/accom.js
@@ -188,6 +188,4 @@ export default function Accommodation(props) {
             <p></p>
         )
     }
-
-
 }
