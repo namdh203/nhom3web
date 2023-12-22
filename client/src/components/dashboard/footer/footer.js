4f0cfccf031808fa289aabe22c1a8c7f573e6ff8
minhhpkp <phamquangminh1659@gmail.com>
2023-12-22 16:43:59 +0700

    Fix small bugs

--- client/src/components/dashboard/footer/footer.js
+++ client/src/components/dashboard/footer/footer.js
@@ -1,78 +1,98 @@
-import React from 'react';
-import "./footer.css"
+import React from "react";
+import "./footer.css";
 
 export default function Footer() {
-    return (
-        <div className="footer-wrapper">
-            <div className="footer-info container-md">
-                <div className="row g-5">
-                    <div className="col col-md-4 col-sm-12 col-12 footer-logo">
-                        <a href='/'>
-                            <img className="footer-logo_img" src="https://i.imgur.com/qm46CNl.png" alt="logo" />
-                        </a>
-                    </div>
-                    <div className="col col-md-4 col-sm-12 col-12 footer-category">
-                        <h3>Category</h3>
-                        <div className="row g-5">
-                            <div className="col col-md-6 col-sm-12 col-12 footer-category_1">
-                                <a href="/?type=destination&id=0">
-                                    <p>Destinations</p>
-                                </a>
-                                <a href="/?type=family&id=1">
-                                    <p>Family tours</p>
-                                </a>
-                                <a href="/?type=honey moon&id=2">
-                                    <p>Honey mons</p>
-                                </a>
-                                <a href="/?type=adventure&id=3">
-                                    <p>Adventures</p>
-                                </a>
-                            </div>
-                            <div className="col col-md-6 col-sm-12 col-12 footer-category_1">
-                                <a href="#">
-                                    <p>Visa-on-arrivals</p>
-                                </a>
-                                <a href="#">
-                                    <p>Special tours</p>
-                                </a>
-                                <a href="#">
-                                    <p>Sales</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col col-md-2 col-sm-12 col-12 footer-social">
-                        <h3>Social</h3>
-                        <a href="#">
-                            <p><i class="fa-brands fa-facebook"></i> Facebook</p>
-                        </a>
-                        <a href="#">
-                            <p><i class="fa-brands fa-instagram"></i> Instagram</p>
-                        </a>
-                        <a href="#">
-                            <p><i class="fa-brands fa-tiktok"></i> Tiktok</p>
-                        </a>
-                        <a href="#">
-                            <p><i class="fa-brands fa-youtube"></i> Youtube</p>
-                        </a>
-                    </div>
-                </div>
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-info container-md">
+        <div className="row g-5">
+          <div className="col col-md-4 col-sm-12 col-12 footer-logo">
+            <a href="/">
+              <img
+                className="footer-logo_img"
+                src="https://i.imgur.com/qm46CNl.png"
+                alt="logo"
+              />
+            </a>
+          </div>
+          <div className="col col-md-4 col-sm-12 col-12 footer-category">
+            <h3>Category</h3>
+            <div className="row g-5">
+              <div className="col col-md-6 col-sm-12 col-12 footer-category_1">
+                <a href="/?type=destination&id=0">
+                  <p>Destinations</p>
+                </a>
+                <a href="/?type=family&id=1">
+                  <p>Family tours</p>
+                </a>
+                <a href="/?type=honey moon&id=2">
+                  <p>Honey mons</p>
+                </a>
+                <a href="/?type=adventure&id=3">
+                  <p>Adventures</p>
+                </a>
+              </div>
+              <div className="col col-md-6 col-sm-12 col-12 footer-category_1">
+                <a href="#">
+                  <p>Visa-on-arrivals</p>
+                </a>
+                <a href="#">
+                  <p>Special tours</p>
+                </a>
+                <a href="#">
+                  <p>Sales</p>
+                </a>
+              </div>
             </div>
-            {/* <div className="footer-aboutus">
+          </div>
+          <div className="col col-md-2 col-sm-12 col-12 footer-social">
+            <h3>Social</h3>
+            <a href="#">
+              <p>
+                <i className="fa-brands fa-facebook"></i> Facebook
+              </p>
+            </a>
+            <a href="#">
+              <p>
+                <i className="fa-brands fa-instagram"></i> Instagram
+              </p>
+            </a>
+            <a href="#">
+              <p>
+                <i className="fa-brands fa-tiktok"></i> Tiktok
+              </p>
+            </a>
+            <a href="#">
+              <p>
+                <i className="fa-brands fa-youtube"></i> Youtube
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <div className="footer-aboutus">
                 <h1 className="aboutus-header">About us</h1>
                 <p className="aboutus-info">We are the some good men that always want to go anywhere, so we develop this app in order to support not only ourself but also everybody who has the same this hobby as us. This application can do a lot of things, from giving you some tour recommendations, better food places or the schedules which suitable for your budget, to help you customize your schedule.</p>
             </div> */}
-            <div className="footer-icon">
-                <div className="footer-icon_wrapper">
-                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-                    <a href="#" style={{ "margin-right": "20px" }}><i class="fa-brands fa-youtube"></i></a>
-                </div>
-            </div>
-            <div className="footer-admin">
-                <p>Nhóm 7 - Web App - INT3306 3</p>
-            </div>
+      <div className="footer-icon">
+        <div className="footer-icon_wrapper">
+          <a href="#">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-tiktok"></i>
+          </a>
+          <a href="#" style={{ marginRight: "20px" }}>
+            <i className="fa-brands fa-youtube"></i>
+          </a>
         </div>
-    )
+      </div>
+      <div className="footer-admin">
+        <p>Nhóm 7 - Web App - INT3306 3</p>
+      </div>
+    </div>
+  );
 }
--- client/src/components/recommend/rcmaccom/rcm-accom.jsx
+++ client/src/components/recommend/rcmaccom/rcm-accom.jsx
@@ -1,106 +1,136 @@
-import React from 'react'
-import "../../../index.css"
-import "./rcm-accom.css"
+import React from "react";
+import "../../../index.css";
+import "./rcm-accom.css";
 
-import RcmAccomCard from './rcm-accom-card.jsx'
-import { getAccomList } from "./RcmAccomFunction.js"
+import RcmAccomCard from "./rcm-accom-card.jsx";
+import { getAccomList } from "./RcmAccomFunction.js";
 
 export default class RcmAccom extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            accomData: null,
-            pagenum: null,
-            card_per_page: 12,
-            pagelist: null
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      accomData: null,
+      pagenum: null,
+      card_per_page: 12,
+      pagelist: null,
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    var page_list = [];
 
-        var page_list = []
-
-        getAccomList(50).then(res => {
-            this.setState({ accomData: res }, () => {
-                for (let i = 0; i < res.length / this.state.card_per_page; i ++) {
-                    page_list.push(i)
-                }
-
-                this.setState({pagelist: page_list})
-            })
-        })
-
-        const currentURL = window.location.href;
-
-        const url = new URL(currentURL);
-
-        var page_num = url.searchParams.get("page_num")
-
-        page_num = parseInt(page_num)
-
-        if (page_num === null) {
-            page_num = 1
+    getAccomList(50).then((res) => {
+      this.setState({ accomData: res }, () => {
+        for (let i = 0; i < res.length / this.state.card_per_page; i++) {
+          page_list.push(i);
         }
 
-        this.setState({pagenum: page_num});
-        // const name = url.searchParams.get("name");
+        this.setState({ pagelist: page_list });
+      });
+    });
 
+    const currentURL = window.location.href;
 
+    const url = new URL(currentURL);
+
+    var page_num = url.searchParams.get("page_num");
+
+    page_num = parseInt(page_num);
+
+    if (isNaN(page_num)) {
+      page_num = 1;
     }
 
-    render() {
+    this.setState({ pagenum: page_num });
+    // const name = url.searchParams.get("name");
+  }
 
-        if (this.state.accomData === null || this.state.pagelist === null || this.state.page_num === null) {
-            return <p>Loading...</p>
-        } else {
-            console.log("Page num: ", this.state.pagenum + 1)
-        }
-
-        return (
-            <div className="rcm-accom_wrapper">
-                <div className="buffer-block" style={{ "height": "51px" }}>
-
-                </div>
-
-                <div className="rcm-banner">
-                    <img src="https://bpb-eu-w2.wpmucdn.com/blogs.lincoln.ac.uk/dist/a/8671/files/2022/04/StMarks-011-by-E-Egg-Joiner-P-small1500.jpg" className="img-fluid rcm-banner_img" alt="Responsive" />
-                    <div className="rcm-banner_intro">
-                        <h2>Recommended Accommodation Options for visitors.</h2>
-                        <p>Here are some of our recommendations for your trip.</p>
-                        <a href="#main">
-                            <div className="nav-btn_wrapper">
-                                <div className="nav-btn">
-                                    <span>View all accommodations   <i class="fa-solid fa-angle-down"></i></span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="rcm-main" id="main">
-                    <div className="rcm-main_wrapper row gx-4 gy-5">
-                        {this.state.accomData.slice((this.state.pagenum - 1) * this.state.card_per_page,
-                        this.state.card_per_page * this.state.pagenum).map(accom => (
-                            <RcmAccomCard accom={accom}></RcmAccomCard>
-                        ))}
-                    </div>
-
-                </div>
-
-                <nav aria-label="Page navigation rcm-pagination">
-                    <ul class="pagination justify-content-center no-margin-bottom pb-40px">
-                        <li class="page-item">
-                            <a class="page-link" href={`?page_num=${Math.max(this.state.pagenum - 1, 1)}`}>Previous</a>
-                        </li>
-                        {this.state.pagelist.map((page) => (
-                            <li class={`page-item${(this.state.pagenum == page + 1) ? " chosen" : ""}`}><a class="page-link" href={`?page_num=${page + 1}`}>{page + 1}</a></li>
-                        ))}
-                        <li class="page-item">
-                            <a class="page-link" href={`?page_num=${Math.min(this.state.pagenum + 1, this.state.pagelist.length)}`}>Next</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
+  render() {
+    if (
+      this.state.accomData === null ||
+      this.state.pagelist === null ||
+      this.state.pagenum === null
+    ) {
+      return <p>Loading...</p>;
+    } else {
+      console.log("Page num: ", this.state.pagenum + 1);
     }
+
+    return (
+      <div className="rcm-accom_wrapper">
+        <div className="buffer-block" style={{ height: "51px" }}></div>
+
+        <div className="rcm-banner">
+          <img
+            src="https://bpb-eu-w2.wpmucdn.com/blogs.lincoln.ac.uk/dist/a/8671/files/2022/04/StMarks-011-by-E-Egg-Joiner-P-small1500.jpg"
+            className="img-fluid rcm-banner_img"
+            alt="Responsive"
+          />
+          <div className="rcm-banner_intro">
+            <h2>Recommended Accommodation Options for visitors.</h2>
+            <p>Here are some of our recommendations for your trip.</p>
+            <a href="#main">
+              <div className="nav-btn_wrapper">
+                <div className="nav-btn">
+                  <span>
+                    View all accommodations{" "}
+                    <i className="fa-solid fa-angle-down"></i>
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="rcm-main" id="main">
+          <div className="rcm-main_wrapper row gx-4 gy-5">
+            {this.state.accomData
+              .slice(
+                (this.state.pagenum - 1) * this.state.card_per_page,
+                this.state.card_per_page * this.state.pagenum
+              )
+              .map((accom) => (
+                <RcmAccomCard accom={accom}></RcmAccomCard>
+              ))}
+          </div>
+        </div>
+
+        <nav aria-label="Page navigation rcm-pagination">
+          <ul className="pagination justify-content-center no-margin-bottom pb-40px">
+            <li key={0} className="page-item">
+              <a
+                className="page-link"
+                href={`?page_num=${Math.max(this.state.pagenum - 1, 1)}`}
+              >
+                Previous
+              </a>
+            </li>
+            {this.state.pagelist.map((page) => (
+              <li
+                key={page + 1}
+                className={`page-item${
+                  this.state.pagenum === page + 1 ? " chosen" : ""
+                }`}
+              >
+                <a className="page-link" href={`?page_num=${page + 1}`}>
+                  {page + 1}
+                </a>
+              </li>
+            ))}
+            <li key={this.state.pagelist.length} className="page-item">
+              <a
+                className="page-link"
+                href={`?page_num=${Math.min(
+                  this.state.pagenum + 1,
+                  this.state.pagelist.length
+                )}`}
+              >
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
