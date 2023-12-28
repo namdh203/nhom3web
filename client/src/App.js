8ec99e83856c2fe35cf4d48b89a01fcfb6371227
ZeroGxMax <21020554@vnu.edu.vn>
2023-12-28 14:38:37 +0700

    Payment

--- client/src/App.js
+++ client/src/App.js
@@ -68,12 +68,13 @@ function App() {
             <Route path="restaurant/res" element={<Restaurant />} />
             <Route path="activity/act" element={<Activity />} />
             <Route path="payment" element={<Payment />} />
-            render={(props) => (
+            <Route path="/payment/payment-step-1" element={
               <div>
                 <Payment />
-                <PaymentStep1 discount={props.location.state?.discount} />
+                <PaymentStep1></PaymentStep1>
+
               </div>
-            )}
+            } />
             <Route
               path="recommendations/accom"
               element={<RcmAccom></RcmAccom>}
